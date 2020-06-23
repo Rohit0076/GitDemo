ca37a15ea72600e0f83863928dc401a8a33dfbbf
SAMSUNG <rohitnjain07@gmail.com>
2020-06-23 12:56:19 +0530

    First commit

---
+++ src/test/java/qaclickacademy/JavaTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class JavaTest {
+
+	
+	@Test
+	public void See()
+	{
+		System.out.println("Hey");
+	}
+
+	
+	
+}
---
+++ src/test/java/qaclickacademy/base.java
@@ -0,0 +1,12 @@
+package qaclickacademy;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class base {
+
+	
+	Properties prop=new Properties();
+	FileInputStream fis=new FileInputStream("C:\\Users\\SAMSUNG\\Mavenjava\\src\\test\\java\\data.properties");
+	
+}
